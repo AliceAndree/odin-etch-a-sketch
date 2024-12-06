eac81c09852d162ac5da69ffb4783b93052523c1
AliceAndree <thiel.alice@gmail.com>
2024-12-06 18:05:46 +0100

    changeSquareAmount() function added

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 let squareAmount = 16;
 const container = document.querySelector(".container");
 const drawingZone = document.createElement("div");
-const gridSquares = document.querySelectorAll(".square");
+const buttonsDiv = document.createElement("div");
+const resetButton = document.createElement("button");
+const clearButton = document.createElement("button");
 
 drawingZone.setAttribute("id", "drawing-zone");
 container.appendChild(drawingZone);
+
+resetButton.textContent = "RESET";
+clearButton.textContent = "CLEAR";
+buttonsDiv.setAttribute("id", "buttons");
+buttonsDiv.appendChild(resetButton);
+buttonsDiv.appendChild(clearButton);
+container.appendChild(buttonsDiv);
 
 function createSquareGrid(squareAmount) {
   for (let i = 0; i < squareAmount ** 2; i++) {
@@ -17,4 +26,23 @@ function createSquareGrid(squareAmount) {
     });
   }
 }
+
+function changeSquareAmount() {
+  squareAmount = prompt("Choose a new grid size from 1 to 100");
+
+  if (
+    squareAmount != null &&
+    !isNaN(parseInt(squareAmount)) &&
+    squareAmount >= 1 &&
+    squareAmount <= 100
+  ) {
+    const gridSquares = document.querySelectorAll(".square");
+    gridSquares.forEach((gridSquare) => {
+      gridSquare.remove();
+    });
+    createSquareGrid(squareAmount);
+  }
+}
+
+resetButton.addEventListener("click", changeSquareAmount);
 createSquareGrid(squareAmount);
